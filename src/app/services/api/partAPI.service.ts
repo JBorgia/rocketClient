52611fe0c8b371b3035b2c17f2b512220ae16422
Jonathan Borgia <jonathanborgia@gmail.com>
2018-07-03 08:27:03 -0600

    create partAPI

--- src/app/services/api/partAPI.service.ts
+++ src/app/services/api/partAPI.service.ts
@@ -9,20 +9,20 @@ export class PartAPI {
     part = {};
     constructor(private http: HttpClient) { }
 
-    getPart(): Observable<any> {
-        return this.http.get('http://localhost:8080/part');
+    getPart(id: string): Observable<any> {
+        return this.http.get(`http://localhost:8080/part/${id}`);
     }
 
     editPart(id, data: object) {
-        return this.http.put<Part>('http://localhost:8080/part/' + id, data);
+        return this.http.put<Part>(`http://localhost:8080/part/${id}`, data);
     }
 
     savePart(review: Part): Observable<Part> {
-        return this.http.post<Part>('http://localhost:8080/part', review);
+        return this.http.post<Part>(`http://localhost:8080/part`, review);
     }
 
     deletePart(id, data: object): Observable<any> {
-        return this.http.delete('http://localhost:8080/part/' + id, data);
+        return this.http.delete(`http://localhost:8080/part/${id}`, data);
     }
 
   // editPart(review: Part): Observable<string> {
